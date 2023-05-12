80efc9e12c2f987a284305f14e7f0741ae3944dc
abdullcadre <95023007+abdullcadre@users.noreply.github.com>
2023-05-12 08:01:09 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <footer class="px-2 py-2 fixed-bottom bg-dark">
   <center>
        <span class="text-muted">Sistema de controlo de presenças criado por 
-            <a class="text-white" >João Antônio dos Anjos</a>       
+            <a class="text-white" >Abdul Cadre</a>       
         </span>
   </center>
        
